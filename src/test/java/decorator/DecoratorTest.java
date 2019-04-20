1afb2770400e788753c5103c296decd53a6ee2e8
CalabashBoy <spyuww@gmail.com>
2019-04-20 14:35:05 +0800

    Create DecoratorTest.java

---
+++ src/test/java/decorator/DecoratorTest.java
@@ -0,0 +1,23 @@
+package decorator;
+
+import org.junit.Test;
+
+/**
+ * @program: design
+ * @description:
+ * @author: Calabash
+ * @create: 2019-04-18 22:07
+ **/
+public class DecoratorTest {
+
+  @Test
+  public void test(){
+
+    Car car = new Car();
+    car.move();
+
+    FlySuperCar flySuperCar = new FlySuperCar(car);
+    flySuperCar.move();
+  }
+
+}
